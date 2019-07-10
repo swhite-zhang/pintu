c39054b2b0b4d92299a266588891b62f58c7c981
zhang <z.h.change@foxmail.com>
2019-07-10 14:41:23 +0800

    Update loadPic.h

--- pintu/loadPic.h
+++ pintu/loadPic.h
@@ -1,20 +1,27 @@
 #pragma once
 #include "framework.h"
-#include "pintu.h"
+#include "jigsaw.h"
 
 class loadPic
-{	
+{
 private:
 	LPCWSTR picPath;
 	int width;
 	int length;
-	int PicX, PicY, DCX, DCY, cutWidth, cutLength;
 	HDC hdc, mdc;
 	HWND hWnd;
-	HBITMAP hbmp;
+
 public:
-	void loadIn(DWORD dword);
-	loadPic(HWND hwnd, LPCWSTR PATH, int x, int y,int picx, int picy, int dcx, int dcy, int  cutWidth, int cutLength) ;
+	HBITMAP hbmp;
+	int PicX, PicY, cutWidth, cutLength;
+	void loadIn(DWORD dword, int dcx, int dcy);
+
+	static void trans(int xstart, int ystart, int xend, int yend, BITMAP bm, unsigned char* px);
+	static void combin(int xstart, int ystart, int xend, int yend, unsigned char* px1, unsigned char* px2, BITMAP bm1, BITMAP bm2, HBITMAP& pic);
+
+
+	loadPic(HWND hwnd, LPCWSTR PATH, int x, int y, int picx, int picy, int  cutWidth, int cutLength);
+
 	~loadPic();
 };
 
